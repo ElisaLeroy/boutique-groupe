c4fef9e61cdacc788aef96936560f9db8f2122f0
Elisa Leroy <elisa.leroy@le-campus-numerique.fr>
2024-08-01 14:48:48 +0200

    création model products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,24 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Products;
 
 class ProductController extends Controller
 {
 
     public function index()
     {
-        $products = DB::select ('select * from products');
-
+        $products = Products::all();
 
         return view("product-list", ["catalogue" => $products]);
     }
 
     public function show(int $id)
     {
-        $products = DB::select ('select * from products');
+        $products = Products::all();
 
         return view("product-details", ["id" => $id, "catalogue" => $products ]);
     }
-
-
+    
 }
