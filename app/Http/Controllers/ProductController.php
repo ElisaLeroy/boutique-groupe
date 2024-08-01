4b7f3624e7102dacf6f021f4f44a509f347faadd
camille-emel <camille.emel@le-campus-numerique.fr>
2024-08-01 16:14:00 +0200

    etape-5

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,17 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
-    public $productList = [1,2,3,4,5,6,7,8,9,10];
+
     public function index()
     {
-        return view("product-list", ["listTitle" => $this->productList]);
+        $data=DB::select('select * from products');
+        return view("product-list", ["products" => $data]);
     }
 
     public function show(int $id)
     {
-        return view("product-details", ["id" => $id]);
+        $datas=DB::select('select * from products WHERE id = :id', ['id' => $id]);
+        return view("product-details", ["datas" => $datas]);
     }//
 }
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,9 +6,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Document</title>
+    <title>
+        @yield('titre')
+    </title>
 </head>
-<body class="min-vh-100">
+
 <div>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
@@ -22,10 +24,10 @@
                         <a class="nav-link active fs-5" aria-current="page" href="{{'/'}}">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active fs-5" aria-current="page" href="{{'cart'}}">Cart</a>
+                        <a class="nav-link active fs-5" aria-current="page" href="{{'/cart'}}">Cart</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link fs-5" href="{{'product'}}">Produit</a>
+                        <a class="nav-link fs-5" href="{{'/product'}}">Produit</a>
                     </li>
                     <li class="nav-item dropdown fs-5">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -50,7 +52,7 @@
         </div>
     </nav>
 </div>
-
+<body class="min-vh-100">
 
 
     @yield('content')
